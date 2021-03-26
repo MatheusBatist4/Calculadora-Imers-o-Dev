7573c2737c2915462853ce60b1df66acc6aff008
Matheus <72423308+MatheusBatist4@users.noreply.github.com>
2021-03-26 15:58:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var primeiroValor = parseFloat(prompt("Digite o primeiro valor: "))
+
+var segundoValor = parseFloat(prompt("Digite o segundo valor: "))
+
+var escolha = prompt("O que deseja fazer? 1-Para adição, 2-Para subtração, 3-Para divisão ou 4-Para multiplicação")
+
+if (escolha == 1) {
+    var resultado = primeiroValor + segundoValor
+    document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+}else if (escolha == 2) {
+  var resultado = primeiroValor - segundoValor
+  document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+}else if (escolha == 3){
+  var resultado = primeiroValor / segundoValor
+  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+}else if (escolha == 4){
+  var resultado = primeiroValor * segundoValor
+  document.write("<h2>" + primeiroValor + " * " + segundoValor + " = " + resultado + "</h2>")
+}
